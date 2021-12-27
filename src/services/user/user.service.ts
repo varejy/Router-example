904d19eda2908c4232fe2e725a36bd856abb5d6a
Vasiliy Budenko <lionvasia@mail.ru>
2021-12-27 20:31:51 +0100

    corrected api domain

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -2,9 +2,11 @@ import { Collection } from '@services/@types/collection.type';
 import { first, map, Observable } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
+const domain = process.env.REACT_APP_API;
+
 class UserService {
   getUserList(page = 1): Observable<UserList> {
-    return ajax.get<UserList>(`https://reqres.in/api/users?page=${page}`).pipe(
+    return ajax.get<UserList>(`${domain}/api/users?page=${page}`).pipe(
       first(),
       map((res) => {
         return res.response;
@@ -12,7 +14,7 @@ class UserService {
     );
   }
   getUserById(id: number): Observable<User> {
-    return ajax.get<{ data: User }>(`https://reqres.in/api/users/${id}`).pipe(
+    return ajax.get<{ data: User }>(`${domain}/api/users/${id}`).pipe(
       first(),
       map((res) => res.response.data)
     );
